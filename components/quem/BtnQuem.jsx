ce0d93dfbcc02692c7b0404492a61a812e67be12
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-08-14 18:04:46 +0100

    Update BtnQuem.jsx

--- components/quem/BtnQuem.jsx
+++ components/quem/BtnQuem.jsx
@@ -7,7 +7,7 @@ export default function BtnQuem(props) {
     
 
     return (
-        <div className="text-[10px] max-w-[80px] text-center text-white shadow  px-2 leading-tight py-2 rounded-md bg-blue-600">
+        <div className="text-[10px] max-w-[80px] text-center text-blue-600 shadow  px-2 leading-tight py-2 rounded-md bg-white">
             <Link href={props.href}>
             <a>{props.label}</a>
             </Link>
