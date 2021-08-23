37b88716b374ed4cf7220c2fbe78ff0d396ea022
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-08-23 20:06:15 +0100

    fix size map mundi

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,8 @@
-import "tailwindcss/tailwind.css";
+// import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
+import "../styles/styles.css";
 import Layout from "../components/Layout";
 import ScrollToTop from "../components/ScrollTop";
-
-
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/teste-logos.jsx
+++ pages/teste-logos.jsx
@@ -4,16 +4,16 @@ import Logo from "../components/quem/Logos";
 
 export default function Logos({ logos }) {
   return (
-    <div className="mx-auto py-10">
+    <div className="mx-auto py-10 ">
       <h2 className="sm:text-3xl text-2xl font-medium text-center text-gray-900 mb-6">
         Membros
       </h2>
-      <div className="relative h-[160px] md:h-[320px] xl:h-[450px] mb-4">
+      <div className="relative w-full mx-auto h-[180px] sm:h-[240px] md:h-[320px] xl:h-[450px] 2xl:h-[550px] mb-4">
         <Image
           src={logos[0].image.url}
           alt="Rede Sem Fronteiras"
           layout="fill"
-          objectFit="contain"
+          objectFit="cover"
           loading="lazy"
         />
       </div>
